ee0e8ed1d8e3f3606af3dde69fdbdbb752cc61bd
NSH517 <tmdgusqufsla@gmail.com>
2026-02-26 18:11:56 +0900

    2/26

--- Board/board/src/main/java/com/aloha/board/domain/Boards.java
+++ Board/board/src/main/java/com/aloha/board/domain/Boards.java
@@ -1,6 +1,9 @@
 package com.aloha.board.domain;
 
 import java.util.Date;
+import java.util.List;
+
+import org.springframework.web.multipart.MultipartFile;
 
 import lombok.Data;
 
@@ -13,6 +16,14 @@ public class Boards {
     private String content;     // 내용
     private Date createdAt;     // 등록일자
     private Date updatedAt;     // 수정일자
+
+    // 파일
+    private MultipartFile mainFile;
+    private List<MultipartFile> files;
+
+    // 파일정보
+    private Files file;             // 메인 파일 1:1
+    private List<Files> fileList;   // 첨부 파일 1:N
 
     public Boards() {
         this.id = java.util.UUID.randomUUID().toString();
